e7f4ea2f42e84f7a6b940da6d653056ebdc3714d
Matt Downey <matthew.r.downey@gmail.com>
2015-05-20 15:16:21 -0400

    This is old code that will probably be scrapped

---
+++ Logic.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Logic : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		//Zones for collision logic
+		//Tiling (World Space) Shaders for zone rendering.
+		//AABB testing for closest boxes
+		//if(inside) then elevator logic
+		//
+	}
+}
