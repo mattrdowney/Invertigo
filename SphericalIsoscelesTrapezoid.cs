bc26a8367618d3953a878047b9a275c9c698f0a7
Matt Downey <matthew.r.downey@gmail.com>
2016-02-25 23:14:16 -0500

    A broken step towards working corners

--- SphericalIsoscelesTrapezoid.cs
+++ SphericalIsoscelesTrapezoid.cs
@@ -11,10 +11,10 @@ using System.Linq;
 //using System.Diagnostics;
 
 [System.Serializable]
-public class SphericalIsoscelesTrapezoid /*TODO: get rid of this in production builds*/ : MonoBehaviour //will become Component
+public class SphericalIsoscelesTrapezoid /* : Component*/ : MonoBehaviour //TODO: get rid of this in production builds
 {
-	/*TODO: make const*/
-	[SerializeField] public SphericalIsoscelesTrapezoid		next; //TODO: add k prefix
+	/*CONSIDER: make const*/
+	[SerializeField] public SphericalIsoscelesTrapezoid		next; //CONSIDER: add k prefix
 	[SerializeField] public SphericalIsoscelesTrapezoid		prev;
 
 	[SerializeField] Vector3								path_center;
@@ -23,20 +23,22 @@ public class SphericalIsoscelesTrapezoid /*TODO: get rid of this in production b
 	[SerializeField] Vector3								arc_left;
 	[SerializeField] Vector3								arc_right; //CONSIDER: can be dropped
 
-	[SerializeField] Vector3								arc_left_up;
-	[SerializeField] Vector3								arc_right_down;
+	[SerializeField] Vector3								arc_left_up; //FIXME: shitty names 
+	[SerializeField] Vector3								arc_right_down; //FIXME
 	
-	[SerializeField] float									arc_radius;
-	[SerializeField] float									arc_angle;
-	
+	[SerializeField] public float							arc_radius; //FIXME: temp hack public
+	[SerializeField] public float							arc_angle; //FIXME:
+
+	static int 												guid = 0;
+
 	/** Determine if the character (represented by a point) is inside of a trapezoid (extruded by the radius of the player)
 	 *  
 	 */
-	public bool Contains(Vector3 pos, float radius_sine)
+	public bool Contains(Vector3 pos, float radius)
 	{
 		float prod = Vector3.Dot(pos - path_center, path_normal);
 
-		bool bIsAtCorrectElevation = 0 <= prod && prod <= radius_sine;
+		bool bIsAtCorrectElevation = 0 <= prod && prod <= radius*Mathf.Sin(radius); //TODO: radius might have changed logic
 		bool bLeftContains		   = Vector3.Dot(pos, arc_left_up ) >= 0;
 		bool bRightContains		   = Vector3.Dot(pos, arc_right_down) >= 0;
 		bool bIsObtuse			   = Vector3.Dot(arc_left, arc_right) <= 0;
@@ -84,8 +86,8 @@ public class SphericalIsoscelesTrapezoid /*TODO: get rid of this in production b
 	{
 		float angle = t / arc_radius; //FIXME: factor in height
 
-		Vector3 adjusted_center = path_center + path_normal*height*     Mathf.Sin(height);
-		float   adjusted_radius = arc_radius  -             height*(1 - Mathf.Cos(height));
+		Vector3 adjusted_center = path_center + path_normal*height*     Mathf.Sin(height); //TODO: check, this might be a hell of a lot more complicated when:
+		float   adjusted_radius = arc_radius  -             height*(1 - Mathf.Cos(height)); //two adjacent SphereIsoTraps are not perpendicular. Might be necessary to do this: http://gis.stackexchange.com/questions/48937/how-to-calculate-the-intersection-of-2-circles
 
 		Vector3 horizontal = adjusted_radius*Mathf.Cos(angle)*arc_left;
 		Vector3 vertical   = adjusted_radius*Mathf.Sin(angle)*arc_left_up;
@@ -145,7 +147,7 @@ public class SphericalIsoscelesTrapezoid /*TODO: get rid of this in production b
 	 * @param normal: the normal plane that intersects lhs and rhs and forms the walking path for the players center
 	 * 		  of mass, sign matters because it indicates which direction is up for calculating the center of mass.
 	 * 
-	 * @example Initialize(Vector3(0,0,1),Vector3(1,0,0), Vector3(0,1,0)) will initialize a Spherical Isosceles Trapezoid
+	 * @example Initialize(Vector3(0,0,1), Vector3(1,0,0), Vector3(0,1,0)) will initialize a Spherical Isosceles Trapezoid
 	 *          that is a great circle for the feet positions, a large lesser circle for the center of mass position,
 	 *          with a 90 degree arc going from forwards to right and a normal going in the positive y-direction.
 	 */
@@ -157,8 +159,8 @@ public class SphericalIsoscelesTrapezoid /*TODO: get rid of this in production b
 		path_normal = normal.normalized; //when at first you don't believe it, pry, pry again
 		path_center = normal*Vector3.Dot(left_edge, normal); //or right_edge
 
-		arc_left  = (left_edge  - path_center).normalized;
-		arc_right = (right_edge - path_center).normalized;
+		arc_left  = (left_edge  - path_center).normalized; //FIXME: obsolete for corner triangles
+		arc_right = (right_edge - path_center).normalized; //FIXME: obsolete
 
 		arc_left_up    = -Vector3.Cross(arc_left , path_normal).normalized; //just in case
 		arc_right_down =  Vector3.Cross(arc_right, path_normal).normalized;
@@ -171,6 +173,33 @@ public class SphericalIsoscelesTrapezoid /*TODO: get rid of this in production b
 		{
 			arc_angle += Mathf.PI;
 		}
+
+		next = prev = this;
+		RecalculateAABB();
+	}
+
+	public void InitializeCorner(SphericalIsoscelesTrapezoid left, SphericalIsoscelesTrapezoid right)
+	{
+		path_center =  right.Evaluate(0);
+		path_normal = -right.Evaluate(0);
+
+		arc_left  = left.arc_right;
+		arc_right = right.arc_left;
+
+		arc_left_up    = -Vector3.Cross(arc_left , path_normal).normalized; //just in case
+		arc_right_down =  Vector3.Cross(arc_right, path_normal).normalized;
+		
+		arc_radius = 0f;
+		
+		arc_angle = Vector3.Angle(arc_left, arc_right) * Mathf.PI / 180;
+		
+		if(Vector3.Dot(arc_left_up, arc_right) <= 0)
+		{
+			arc_angle += Mathf.PI;
+		}
+
+		this.Relink(left, right);
+		RecalculateAABB();
 	}
 
 	/** Find the point of collision as a parameterization of a circle.
@@ -185,13 +214,13 @@ public class SphericalIsoscelesTrapezoid /*TODO: get rid of this in production b
 
 		secant.Normalize();
 
-		Vector3 adj_center = path_center + path_normal*(height*Mathf.Sin(height)); //TODO: - for normals towards the origin
-		float   adj_radius = arc_radius - (height - height*Mathf.Cos(height)); //TODO: + for normals pointing towards the origin 
+		Vector3 adjusted_center = path_center + path_normal*(height*Mathf.Sin(height)); //TODO: check
+		float   adjusted_radius = arc_radius - height*(1 - Mathf.Cos(height)); //TODO: check 
 
-		Vector3 intersection = adj_center + secant*adj_radius;
+		Vector3 intersection = adjusted_center + secant*adjusted_radius;
 
-		float x = Vector3.Dot(intersection, arc_left   ) / adj_radius;
-		float y = Vector3.Dot(intersection, arc_left_up) / adj_radius;
+		float x = Vector3.Dot(intersection, arc_left   ) / adjusted_radius;
+		float y = Vector3.Dot(intersection, arc_left_up) / adjusted_radius;
 		
 		float angle = Mathf.Atan2(y,x);
 
@@ -207,13 +236,28 @@ public class SphericalIsoscelesTrapezoid /*TODO: get rid of this in production b
 		return new Optional<float>();
 	}
 
+	public SphericalIsoscelesTrapezoid LinkLeft(Vector3 pos)
+	{
+		Vector3 left = this.Evaluate(0);
+
+		SphericalIsoscelesTrapezoid obj = SphericalIsoscelesTrapezoid.Spawn(pos, left, Vector3.Cross(pos, left));
+
+		return obj.Relink(prev, this);
+	}
+
+	public SphericalIsoscelesTrapezoid LinkRight(Vector3 pos)
+	{
+		Vector3 right = this.Evaluate(arc_angle*arc_radius);
+
+		SphericalIsoscelesTrapezoid obj = SphericalIsoscelesTrapezoid.Spawn(right, pos, Vector3.Cross(right, pos));
+
+		return obj.Relink(this, next);
+	}
+
 	Vector3 MaxGradient(Vector3 desired)
 	{
 		Vector3 max_gradient = Vector3.zero;
 		float max_product = Mathf.NegativeInfinity;
-
-		Debug.Log(Evaluate(0f));
-		Debug.Log(Evaluate(arc_angle*arc_radius));
 
 		/** if we don't calculate per quadrant, calculations for an arc with angle 2*PI become ambiguous because left == right
 		 */ 
@@ -285,8 +329,10 @@ public class SphericalIsoscelesTrapezoid /*TODO: get rid of this in production b
 	 * 
 	 *  @param collider the box collider that will be altered to contain the SphericalIsoscelesTrapezoid
 	 */
-	public void RecalculateAABB(BoxCollider collider)
+	public void RecalculateAABB()
 	{
+		BoxCollider	collider = this.GetComponent<BoxCollider>(); 
+
 		float x_min = MaxGradient(Vector3.left   ).x;
 		float x_max = MaxGradient(Vector3.right  ).x;
 		float y_min = MaxGradient(Vector3.down   ).y;
@@ -301,6 +347,53 @@ public class SphericalIsoscelesTrapezoid /*TODO: get rid of this in production b
 		collider.size   = new Vector3( x_max - x_min,
 									   y_max - y_min,
 									   z_max - z_min);
+	}
+
+	public SphericalIsoscelesTrapezoid Relink(SphericalIsoscelesTrapezoid left, SphericalIsoscelesTrapezoid right)
+	{
+		this.next  = right;
+		this.prev  = left;
+
+		left.next  = this;
+		right.prev = this;
+
+		return this;
+	}
+
+	
+	public static SphericalIsoscelesTrapezoid Spawn()
+	{
+		GameObject prefab = (GameObject) Resources.Load("SphereIsoTrap");
+		
+		#if UNITY_EDITOR
+		GameObject obj = UnityEditor.PrefabUtility.InstantiatePrefab(prefab) as GameObject;
+		#else
+		GameObject obj = Instantiate(prefab) as GameObject;
+		#endif
+
+		prefab.name = guid.ToString();
+
+		guid++;
+		
+		return obj.GetComponent<SphericalIsoscelesTrapezoid>();
+	}
+
+	public static SphericalIsoscelesTrapezoid Spawn(Vector3 left_edge, Vector3 right_edge, Vector3 normal)
+	{
+		SphericalIsoscelesTrapezoid trapezoid = Spawn();
+		
+		trapezoid.Initialize(left_edge, right_edge, normal);
+		
+		return trapezoid; //used for next/prev
+	}
+
+	public static SphericalIsoscelesTrapezoid SpawnCorner(SphericalIsoscelesTrapezoid left, SphericalIsoscelesTrapezoid right)
+	{
+		SphericalIsoscelesTrapezoid trapezoid = Spawn();
+
+		trapezoid.InitializeCorner(left, right);
+
+		return trapezoid; //used for next/prev
 	}
 
 	/** Counts the number of booleans that are true in a comma separated list of booleans
