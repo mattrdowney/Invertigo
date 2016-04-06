d0decfbff653c8999f2641630f2d8779b0695166
Matt Downey <matthew.r.downey@gmail.com>
2016-04-06 01:43:47 -0400

    Polar grid snapping working

--- CharacterMotor.cs
+++ CharacterMotor.cs
@@ -217,7 +217,7 @@ public class CharacterMotor : MonoBehaviour //TODO: make abstract //CONSIDER: ma
 			Vector3 input3D = new Vector3(input.x, input.y, 0f); //FIXME: JANK
 			if(input3D.sqrMagnitude > 1) input3D.Normalize();
 
-			angle += Vector3.Dot(camera_transform.rotation*input3D, right) / height / 64;
+			angle += Vector3.Dot(camera_transform.rotation*input3D, right) / height / 64; //FIXME: slight math error here-ish
 
 			transform.position = ArcOfSphere.Evaluate(ground.data, radius);
 
--- Invertigo.cs
+++ Invertigo.cs
@@ -1,4 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Invertigo : MonoBehaviour { }
+public class Invertigo : MonoBehaviour
+{
+	public int rows = 8;
+	public int columns = 16;
+
+	void OnDrawGizmos()
+	{
+		UnityEditor.Handles.color = Color.white;
+
+		for(float row = 0f; row <= 1f; row += 1f/columns)
+		{
+			UnityEditor.Handles.DrawWireDisc(Vector3.down*Mathf.Cos(Mathf.PI*row), Vector3.up, Mathf.Sin(Mathf.PI*row));
+		}
+		
+		for(float column = 0f; column <= 1f; column += 1f/rows)
+		{
+			UnityEditor.Handles.DrawWireDisc(Vector3.zero,
+			                                 Vector3.forward*Mathf.Cos(Mathf.PI*column) +
+			                                 Vector3.right  *Mathf.Sin(Mathf.PI*column),
+			                                 1);
+		}
+	}
+}
