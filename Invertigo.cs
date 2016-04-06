a28835e2082bf98904767202a2440e0bc15228f4
Matt Downey <matthew.r.downey@gmail.com>
2016-04-06 02:41:48 -0400

    minor fixes to row / col of polar snapping

--- Invertigo.cs
+++ Invertigo.cs
@@ -3,23 +3,25 @@ using System.Collections;
 
 public class Invertigo : MonoBehaviour
 {
-	public int rows = 8;
-	public int columns = 16;
+	public int rows = 15; //equator drawn when rows is odd
+	public int columns = 8; //RENAME?: misleading //always draws 2*columns lines from the North to South pole
 
 	void OnDrawGizmos()
 	{
 		UnityEditor.Handles.color = Color.white;
 
-		for(float row = 0f; row <= 1f; row += 1f/columns)
+		for(float row = 1; row <= rows; ++row)
 		{
-			UnityEditor.Handles.DrawWireDisc(Vector3.down*Mathf.Cos(Mathf.PI*row), Vector3.up, Mathf.Sin(Mathf.PI*row));
+			UnityEditor.Handles.DrawWireDisc(Vector3.down*Mathf.Cos(Mathf.PI*row/(rows+1)),
+			                                 Vector3.up,
+			                                 Mathf.Sin(Mathf.PI*row/(rows+1)));
 		}
 		
-		for(float column = 0f; column <= 1f; column += 1f/rows)
+		for(float column = 0; column < columns; ++column)
 		{
 			UnityEditor.Handles.DrawWireDisc(Vector3.zero,
-			                                 Vector3.forward*Mathf.Cos(Mathf.PI*column) +
-			                                 Vector3.right  *Mathf.Sin(Mathf.PI*column),
+			                                 Vector3.forward*Mathf.Cos(Mathf.PI*column/columns) +
+			                                 Vector3.right  *Mathf.Sin(Mathf.PI*column/columns),
 			                                 1);
 		}
 	}
