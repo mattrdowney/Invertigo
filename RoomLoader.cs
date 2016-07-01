f8512e5a7375e7a56425a6f1faaea881744ead04
Matt Downey <matthew.r.downey@gmail.com>
2016-07-01 11:49:13 -0400

    BasicRoomLoader fully functional minus Nexuses

--- RoomLoader.cs
+++ RoomLoader.cs
@@ -7,6 +7,8 @@ class RoomLoader : MonoBehaviour //use Strategy Pattern
 {
     IRoomLoader strategy;
 
+    int initial_level = 1;
+
     public void Start()
     {
         strategy = new BasicRoomLoader();
@@ -16,8 +18,27 @@ class RoomLoader : MonoBehaviour //use Strategy Pattern
         StartCoroutine(Wait());
     }
 
+    private void LoadRoom(int level)
+    {
+        GameObject root = GameObject.Find("/" + level.ToString());
+
+        root.SetActiveRecursively(true);
+    }
+
+    private void UnloadRoom(int level)
+    {
+        GameObject root = GameObject.Find("/" + level.ToString());
+
+        root.SetActiveRecursively(false); // make all objects stop updating inside room
+        root.SetActive(true); //keep the root object loaded so it can be found with GameObject.Find
+    }
+
     IEnumerator Wait()
     {
         yield return new WaitUntil(() => strategy.IsDone());
+
+        LoadRoom(initial_level);
+
+        Time.timeScale = 1;
     }
 }
