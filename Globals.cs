d86e70401ad6181aa82ed461fc5e9094104868d7
Matt Downey <matthew.r.downey@gmail.com>
2016-07-03 20:19:32 -0400

    simple SVGBuilder

--- Globals.cs
+++ Globals.cs
@@ -4,4 +4,14 @@ using System.Collections;
 public static class Globals
 {
 	public static float radius = 0.5f;
+
+    //8 subdivisions latitude (final BOSS)
+    //+16 subdivisions (final corridor)
+    //24 subdivisions (final stage)
+    //+8 subdivisions corridor
+    //32
+    //40
+    //48
+    //56
+    //64 subdivisions (first stage)
 }
---
+++ SVGBuilder.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+
+using UnityEngine;
+using System.Collections;
+
+public class SVGBuilder : MonoBehaviour //DOESN'T need to be MonoBehaviour
+{
+    static StreamWriter writer;
+    static bool first;
+
+	void Start ()
+    {
+        WriteHeader();
+	}
+
+    void OnDestroy()
+    {
+        WriteFooter();
+    }
+
+    public static void BeginPoint()
+    {
+        first = true;
+        writer.Write("\t<path d=\"");
+    }
+
+    public static void SetPoint(float x, float y)
+    {
+        x *= 1000;
+        y *= 1000;
+
+        if (first)
+        {
+            writer.Write("M ");
+            first = false;
+        }
+        else
+        {
+            writer.Write(" L ");
+        }
+
+        writer.Write(x + " " + y);
+    }
+
+    public static void EndPoint()
+    {
+        writer.Write("\"/>\n");
+    }
+
+    static void WriteHeader()
+    {
+        writer = new StreamWriter("C:/Users/Matt Downey/Documents/Invertigo/Assets/TestSVG.svg");
+        writer.Write("<svg width=\"1000\" height=\"1000\">\n\n");
+    }
+
+    static void WriteFooter()
+    {
+        writer.Write("\n</svg>");
+        writer.Flush();
+    }
+}
